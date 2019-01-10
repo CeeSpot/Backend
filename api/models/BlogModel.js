5b76ad3f52d59463aa54f9976b7f7fda2ce4aaf0
KYLIAN-PC\KYLIAN <kylianleemkuil@hotmail.com>
2019-01-10 23:40:00 +0100

    getBlogs doesnt get the body now

--- api/models/BlogModel.js
+++ api/models/BlogModel.js
@@ -4,7 +4,7 @@ var config = require('../config');
 module.exports = {
     getBlogs: function (req) {
         return new Promise(function (resolve, reject) {
-            con.query(`SELECT * FROM blogs`, function (err, res) {
+            con.query(`SELECT id, title, description, picture, author, date_created FROM blogs`, function (err, res) {
                 if (err) {
                     reject({
                         success: false,
