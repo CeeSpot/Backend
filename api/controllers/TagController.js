89e99dc0b50e1b7c337bf0b71538fd1117644915
KYLIAN-PC\KYLIAN <kylianleemkuil@hotmail.com>
2019-01-22 17:01:03 +0100

    Added create tag / edit tag / delete tag

--- api/controllers/TagController.js
+++ api/controllers/TagController.js
@@ -17,3 +17,27 @@ exports.getCompanyTags = function (req, res) {
         res.send(err);
     });
 };
+
+exports.addTag = function (req, res) {
+    tagModel.addTag(req).then(function (data) {
+        res.send(data);
+    }).catch(function (err) {
+        res.send(err);
+    });
+};
+
+exports.editTag = function (req, res) {
+    tagModel.editTag(req).then(function (data) {
+        res.send(data);
+    }).catch(function (err) {
+        res.send(err);
+    });
+};
+
+exports.deleteTag = function (req, res) {
+    tagModel.deleteTag(req).then(function (data) {
+        res.send(data);
+    }).catch(function (err) {
+        res.send(err);
+    });
+};
